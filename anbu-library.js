2ca74a56c29892f11266de5655abb0413fe86025
Hanyaku <collinbuchkamer@gmail.com>
2023-01-15 03:52:37 +0100

    Upload

---
+++ anbu-library.js
@@ -0,0 +1,83 @@
+const net = require('net');
+
+class Database {
+
+  constructor(host = 'localhost', port = 8000, password) {
+
+    this.host = host;
+
+    this.port = port;
+
+    this.password = password;
+
+  }
+
+  _send(message) {
+
+    return new Promise((resolve, reject) => {
+
+      const client = net.createConnection({ host: this.host, port: this.port }, () => {
+
+        client.write(`${this.password}:${message}`);
+
+      });
+
+  
+
+      let data = '';
+
+  
+
+      client.on('data', (chunk) => {
+
+        data += chunk;
+
+      });
+
+  
+
+      client.on('end', () => {
+
+        resolve(JSON.parse(data));
+
+      });
+
+  
+
+      client.on('error', (err) => {
+
+        reject(err);
+
+      });
+
+    });
+
+  }
+
+  async get(key) {
+
+    const result = await this._send(`GET:${key}`);
+
+    return result;
+
+  }
+
+  async set(key, value) {
+
+    const result = await this._send(`POST:${key}:${value}`);
+
+    return result;
+
+  }
+
+  async delete(key) {
+
+    const result = await this._send(`DELETE:${key}`);
+
+    return result;
+
+  }
+
+}
+
+module.exports = Database;
